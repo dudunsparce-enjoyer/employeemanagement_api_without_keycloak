320fb271890e4d0a9fa5193834667cf587e8cb35
mreinhardt <mreinhardt@neusta.de>
2024-01-18 10:15:02 +0100

    added unfinished detail to detailed-view.component

--- src/app/detailed-view/detailed-view.component.ts
+++ src/app/detailed-view/detailed-view.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {EmployeeListComponent} from "../employee-list/employee-list.component";
+import {Observable, of} from "rxjs";
+import {Employee} from "../Employee";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-detailed-view',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./detailed-view.component.css']
 })
 export class DetailedViewComponent {
+
+  employees$: Observable<Employee[]>;
+
+  constructor(private http: HttpClient) {
+    this.employees$ = of([]);
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.employees$ = this.http.get<Employee[]>('http://localhost:8089/employees', {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
+    });
+  }
 
 }
