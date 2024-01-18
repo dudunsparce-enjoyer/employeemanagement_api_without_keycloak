3a3422c32c2de71f49a01184a5cede0642c6e5df
mreinhardt <mreinhardt@neusta.de>
2024-01-18 10:49:32 +0100

    extracted routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,25 @@ import {Routes, RouterModule, Route} from '@angular/router';
 import{EmployeeListComponent} from "./employee-list/employee-list.component";
 import {DetailedViewComponent} from "./detailed-view/detailed-view.component";
 
+const employeeRoute: Route = {
+  path: 'employee',
+  component: EmployeeListComponent
+}
+
 const detailedViewRoute: Route = {
   path: 'employee/:id',
   component: DetailedViewComponent
 }
 
+const invalidRoute: Route = {
+  path: '**',
+  redirectTo: employeeRoute.path,
+  pathMatch: "full"
+}
+
 const routes: Routes = [
-  { path: '**', redirectTo: '/employee', pathMatch: 'full' },
-  { path: 'employee', component: EmployeeListComponent},
+  invalidRoute,
+  employeeRoute,
   detailedViewRoute,
 ];
 
